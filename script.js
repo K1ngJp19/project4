806b4e7e5cf3f851ded601abd506862c9f247913
jpcu2 <johnpaulcu19@gmail.com>
2024-07-29 18:43:06 +0800

    Initial commit

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
 let highestZ = 1;
 
 class Paper {
-  holdingPaper = false;
-  mouseTouchX = 0;
-  mouseTouchY = 0;
-  mouseX = 0;
-  mouseY = 0;
-  prevMouseX = 0;
-  prevMouseY = 0;
-  velX = 0;
-  velY = 0;
-  rotation = Math.random() * 30 - 15;
-  currentPaperX = 0;
-  currentPaperY = 0;
-  rotating = false;
+  constructor() {
+    this.holdingPaper = false;
+    this.mouseTouchX = 0;
+    this.mouseTouchY = 0;
+    this.mouseX = 0;
+    this.mouseY = 0;
+    this.prevMouseX = 0;
+    this.prevMouseY = 0;
+    this.velX = 0;
+    this.velY = 0;
+    this.rotation = Math.random() * 30 - 15;
+    this.currentPaperX = 0;
+    this.currentPaperY = 0;
+    this.rotating = false;
+  }
 
   init(paper) {
     document.addEventListener('mousemove', (e) => {
-      if(!this.rotating) {
+      if (!this.rotating) {
         this.mouseX = e.clientX;
         this.mouseY = e.clientY;
-        
+
         this.velX = this.mouseX - this.prevMouseX;
         this.velY = this.mouseY - this.prevMouseY;
       }
-        
+
       const dirX = e.clientX - this.mouseTouchX;
       const dirY = e.clientY - this.mouseTouchY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
       const dirNormalizedX = dirX / dirLength;
       const dirNormalizedY = dirY / dirLength;
 
       const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
-      let degrees = 180 * angle / Math.PI;
+      let degrees = (180 * angle) / Math.PI;
       degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
+      if (this.rotating) {
         this.rotation = degrees;
       }
 
-      if(this.holdingPaper) {
-        if(!this.rotating) {
+      if (this.holdingPaper) {
+        if (!this.rotating) {
           this.currentPaperX += this.velX;
           this.currentPaperY += this.velY;
         }
@@ -48,25 +50,26 @@ class Paper {
 
         paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
       }
-    })
+    });
 
     paper.addEventListener('mousedown', (e) => {
-      if(this.holdingPaper) return; 
+      if (this.holdingPaper) return;
       this.holdingPaper = true;
-      
+
       paper.style.zIndex = highestZ;
       highestZ += 1;
-      
-      if(e.button === 0) {
+
+      if (e.button === 0) {
         this.mouseTouchX = this.mouseX;
         this.mouseTouchY = this.mouseY;
         this.prevMouseX = this.mouseX;
         this.prevMouseY = this.mouseY;
       }
-      if(e.button === 2) {
+      if (e.button === 2) {
         this.rotating = true;
       }
     });
+
     window.addEventListener('mouseup', () => {
       this.holdingPaper = false;
       this.rotating = false;
@@ -74,9 +77,18 @@ class Paper {
   }
 }
 
-const papers = Array.from(document.querySelectorAll('.paper'));
+document.addEventListener('DOMContentLoaded', (event) => {
+  const backgroundMusic = document.getElementById('backgroundMusic');
 
-papers.forEach(paper => {
-  const p = new Paper();
-  p.init(paper);
+  document.addEventListener('click', () => {
+    backgroundMusic.play().catch((error) => {
+      console.log('Autoplay was prevented:', error);
+    });
+  });
+
+  const papers = Array.from(document.querySelectorAll('.paper'));
+  papers.forEach((paper) => {
+    const p = new Paper();
+    p.init(paper);
+  });
 });
